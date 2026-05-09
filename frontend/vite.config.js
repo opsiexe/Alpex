b39a613f9654001941bccbcb680a2e179988d17e
opsiexe <dorseuilmatheo@gmail.com>
2026-05-09 08:31:39 +0400

    refactor: replace placeholder content with main app structure, integrate routing, and update styling with TailwindCSS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      tailwindcss(),
+      react(),
+  ],
+  server: {
+    proxy: {
+      // redirige /api et /ws vers FastAPI en dev
+      '/api': 'http://localhost:8000',
+      '/ws': { target: 'ws://localhost:8000', ws: true }
+    }
+  }
 })
